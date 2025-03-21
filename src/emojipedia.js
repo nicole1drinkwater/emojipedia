7c0448c4cfc0aa7643003da5bcc5f1f695f32146
Nicole Drinkwater <129891641+nicole1drinkwater@users.noreply.github.com>
2025-03-21 14:42:19 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,39 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "'You can do that!' or 'I feel strong!' Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Can mean you're saying a prayer, or hoping for enlightenment. Is also used as a 'high five' or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "😂",
+    name: "Laughing Face",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of 'rofl'. Stands for 'rolling on the floor, laughing'.",
+  },
+  {
+    id: 4,
+    emoji: "🔥",
+    name: "Fire",
+    meaning:
+      "A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame. Commonly used for various metaphorical expressions related to fire, including the slang hot (“attractive”) and lit (“excellent”).",
+  },
+  {
+    id: 5,
+    emoji: "😶‍🌫️",
+    name: "Face in Clouds",
+    meaning:
+      "A face that appears surrounded by clouds or a haze of smoke. Ambiguous in appearance, this may represent a foggy state of mind, confusion, or even a sense of calm bliss. May also be used to indicate the presence of smoke.",
+  },
+];
+
+export default emojipedia;
